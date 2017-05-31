ff4bf4c467953c6beebb97b0eb34a00dbece492f
PedroAmat <amat96pedro@gmail.com>
2017-05-31 14:10:06 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -16,6 +16,27 @@ $(document).ready(function () {
     $("#scrollup").click(function () {
         $('html, body').animate({scrollTop: 0}, 'slow');
     });
+
+    $("#face").click(function () {
+        var shareurl = window.location.href;
+        window.open('https://www.facebook.com/sharer/sharer.php?u=' + escape(shareurl), document.title,
+                'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+        return false;
+    });
+
+    $("#twit").click(function () {
+        var shareurl = window.location.href;
+        window.open('http://twitter.com/share?text=' + document.title + '&url=' + escape(shareurl), document.title,
+                'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+        return false;
+    });
+
+    $("#gplus").click(function () {
+        var shareurl = window.location.href;
+        window.open('https://plus.google.com/share?url=' + escape(shareurl), document.title,
+                'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+        return false;
+    });
 });
 
 function createNews(json) {
@@ -45,3 +66,6 @@ function loadNews() {
         alert("You've just seen all our news!");
     }
 }
+
+
+
